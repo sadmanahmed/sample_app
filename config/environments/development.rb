358ea174f86c977abf2006dde6c2331814d75a0d
sadmanahmed <abirsadmanahmed177@gmail.com>
2020-01-12 21:07:14 +0600

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,8 +31,22 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+
+
+  host = 'localhost:3000' # Don't use this literally; use your local dev host instead
+  # Use this on the cloud IDE.
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+  # Use this if developing on localhost.
+  # config.action_mailer.default_url_options = { host: host, protocol: 'http' }
+
+
+
+
+
 
   config.action_mailer.perform_caching = false
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,10 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar", admin: true)
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 # Generate a bunch of additional users.
 99.times do |n|
@@ -17,7 +20,9 @@ User.create!(name:  "Example User",
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
